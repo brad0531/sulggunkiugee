f7ae08202ad2ffa388754562ed75fe38abe9ffe2
1mxn107 <uu254664@gmail.com>
2025-07-08 00:11:06 +0900

    리스폰

--- My project/Assets/Jimin/Script/Stage1Manager.cs
+++ My project/Assets/Jimin/Script/Stage1Manager.cs
@@ -4,19 +4,30 @@ using UnityEngine.SceneManagement;
 public class Stage1Manager : MonoBehaviour
 {
     public GameObject Monster;
+    public Vector2 PlayerRespawnPoint;
 
     void SpawnMonster()
     {
         Vector3 playerPosition = GameObject.FindWithTag("Player").transform.position;
         GameObject enemy = (GameObject)Instantiate(Monster, new Vector3(playerPosition.x + 1000, playerPosition.y), Quaternion.identity);
         for (int i = 1; i < 10; i++) {
-            GameObject enemys = (GameObject)Instantiate(Monster, new Vector3(playerPosition.x +1000 + 500*i, playerPosition.y), Quaternion.identity);
+            GameObject enemys = (GameObject)Instantiate(Monster, new Vector3(playerPosition.x +1500 + 1200*i, playerPosition.y), Quaternion.identity);
         }
+    }
+
+    void Respawn()
+    {
+        //if() //스테이지 이동
+        {
+            GameObject.FindWithTag("Player").transform.position = PlayerRespawnPoint;
+        }
+         
     }
 
     void Start()
     {
-        Invoke("SpawnMonster", 3f);
+        Respawn();
+        Invoke("SpawnMonster", 1f);
     }
 
     void Update()
