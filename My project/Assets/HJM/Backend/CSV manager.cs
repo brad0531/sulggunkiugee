b52901d91da1c569ccbf7839bc62401e67ee0ed0
heohuhu <nicejunmin@daum.net>
2025-07-14 23:16:10 +0900

    버그 발생?
    뭐임

--- My project/Assets/HJM/Backend/CSV manager.cs
+++ My project/Assets/HJM/Backend/CSV manager.cs
@@ -5,18 +5,19 @@ using System.Linq;
 using System;
 using System.Data;
 using System.Linq.Expressions;
-public class Utility
+public class GameUtility
 {
     public long get_times() //utc 시간 (표준 시간)
     {
         return DateTime.UtcNow.Ticks;
     }
+
 }
 public class GameManager : MonoBehaviour
 {
 
     #region 데이터 선언
-    private Utility utility = new Utility();
+    private GameUtility utility = new GameUtility();
     public static GameManager Instance { get; private set; }
     private bool isTesting = true;
     [Header("CSV 파일 상대 경로 (StreamingAssets 기준)")]
@@ -593,4 +594,4 @@ public class Pair<T, U>
         return new Tuple<T, U>(this.First, this.Second);
     }
 
-};
+}
