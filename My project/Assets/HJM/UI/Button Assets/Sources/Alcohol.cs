6f8d2138650a2d45fa87016f92ffc29984c2f074
heohuhu <nicejunmin@daum.net>
2025-07-22 01:10:45 +0900

    술 추가, 버튼 연결

--- My project/Assets/HJM/UI/Button Assets/Sources/Alcohol.cs
+++ My project/Assets/HJM/UI/Button Assets/Sources/Alcohol.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Alcohol : MonoBehaviour
 {
-    public void BuyAlcohol(GameManager.Alcohol_index index)
+    public void BuyAlcohol(int index)
     {
+        GameManager.Alcohol_index idx = (GameManager.Alcohol_index)index;
         int money = GameManager.Instance.getMoney();
-        int cost = GameManager.Instance.Load_Alcohol_Cost(index);
+        int cost = GameManager.Instance.Load_Alcohol_Cost(idx);
 
         if (money < cost) //돈 부족
             return;
 
-        if (!GameManager.Instance.isCoolTimeEnd(index))
+        if (!GameManager.Instance.isCoolTimeEnd(idx))
             return;
 
         money -= cost;
