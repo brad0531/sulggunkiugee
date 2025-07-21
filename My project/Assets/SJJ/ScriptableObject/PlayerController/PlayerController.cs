eff8db55b6d625ab5c989d3397241f0d73bd1d77
brad0531 <brad05310531@gmail.com>
2025-07-22 01:13:19 +0900

    몬스터 할당 수정, 함수, 임포트
    수정 사항
    몬스터의 인덱스가 없는 경우 스테이지를 ++해서 할당합니다.
    캐릭터와 몬스터의 크기가 커졌습니다.
    플레이어의 속도가 5f -> 100f가 되었습니다.
    
    추가 사항
     2D Aseprite Importer 를 언락했습니다. 다들 언락 부탁드려요.
    
    추후 개발 사항
    공격, 이동 등의 애니메이션 작업
    attackrange 등의 변수 조정 작업

--- My project/Assets/SJJ/ScriptableObject/PlayerController/PlayerController.cs
+++ My project/Assets/SJJ/ScriptableObject/PlayerController/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public FadeInOut fadeInOut;
 
-    public float moveSpeed = 5f; // 예시 이동속도
+    public float moveSpeed = 100f; // 예시 이동속도
     public float attackRange = 3f; // 예시 공격거리(몬스터와 만나는 거리)
     public float moveDistance = 30f; // 적 처치 후 X축으로 이동할 거리(몬스터 간격)
     public int PlayerHP;
@@ -35,31 +35,7 @@ public class PlayerController : MonoBehaviour
         PlayerHP = GameManager.Instance.getHP();
         PlayerattackPower = GameManager.Instance.getATK();
         Debug.Log($"플레이어의 체력과 공격력을 불러옵니다. 현재 체력은 {PlayerHP}, 공격력은 {PlayerattackPower}입니다.");
-
-        // "Monster" 태그가 붙은 모든 오브젝트의 Transform을 리스트에 저장
-        GameObject[] monsterObjects = GameObject.FindGameObjectsWithTag("Monster");
-        monsters = new List<Transform>();
-        // X좌표 기준 오름차순 정렬
-    System.Array.Sort(monsterObjects, (a, b) =>
-        a.transform.position.x.CompareTo(b.transform.position.x)
-    );
-        // 몬스터 인덱스 할당, 위치 조정을 여기서 해도 좋을 듯
-        for (int  i = 0; i < monsterObjects.Length; i++)
-        {
-            if (!GameManager.Instance.isVaildStage(new Tuple<int, int>(GameManager.Instance.getStage().Item1, i)))
-                continue;
-            GameObject obj = monsterObjects[i];
-            monsters.Add(obj.transform);
-            MonsterController mc = obj.GetComponent<MonsterController>();
-            if (mc != null)
-            {
-                mc.monsterIndex = i;
-                Debug.Log($"[몬스터 등록] 이름: {obj.name}, index: {i}, X: {obj.transform.position.x}");
-            }
-        }
-
-        // 몬스터 사망 이벤트 구독 추가
-        MonsterController.IsMonsterDie += OnMonsterDie;
+        RegisterMonsters();
     }
     void Update()
     {
@@ -99,17 +75,21 @@ public class PlayerController : MonoBehaviour
     void MonsterAttack()
     {
         MonsterController monsterCtrl = CurrentMonster.GetComponent<MonsterController>();
+        PlayerHP = GameManager.Instance.getHP();
+        // 유저가 강화하는 걸 대비, HP 설정 후 뎀지 계산
         PlayerHP -= monsterCtrl.GetATK();
         Debug.Log($"몬스터가 플레이어를 공격! 데미지: {monsterCtrl.GetATK()}, 플레이어 남은 체력: {PlayerHP}");
         if (PlayerHP <= 0)
         {
             PlayerDie();
         }
+        GameManager.Instance.setHP(PlayerHP);
     }
     void PlayerAttack()
     {
         // 데미지 계산
         PlayerattackPower = GameManager.Instance.getATK();
+        
         int damage = PlayerattackPower;
 
         // 몬스터에 데미지 적용
@@ -118,13 +98,12 @@ public class PlayerController : MonoBehaviour
         {
             monsterCtrl.MonsterTakeDamage(damage);
         }
-
-
         // 공격 애니메이션 실행
         if (animator != null)
         {
             animator.SetTrigger("Attack");
         }
+
         // 콘솔에 데미지 결과 출력
         Debug.Log($"플레이어가 몬스터를 공격! 데미지: {damage}, 몬스터 남은 체력: {monsterCtrl.GetCurrentHP()}");
     }
@@ -160,4 +139,56 @@ public class PlayerController : MonoBehaviour
     {
         return Mathf.Abs(CurrentMonster.position.x - transform.position.x);
     }
+
+    private void RegisterMonsters()
+    {
+        // 현재 스테이지 번호 가져오기
+        int stageNum = GameManager.Instance.getStage().Item1;
+        int monsterIndex = 0;
+
+        // "Monster" 태그를 가진 모든 오브젝트 가져오기
+        GameObject[] monsterObjects = GameObject.FindGameObjectsWithTag("Monster");
+        monsters = new List<Transform>();
+
+        // X좌표 기준 오름차순 정렬
+        System.Array.Sort(monsterObjects, (a, b) =>
+            a.transform.position.x.CompareTo(b.transform.position.x)
+        );
+
+        for (int i = 0; i < monsterObjects.Length; i++)
+        {
+            // 유효한 (stageNum, monsterIndex)를 찾을 때까지 다음 스테이지로 넘김
+            while (!GameManager.Instance.isVaildStage(new Tuple<int, int>(stageNum, monsterIndex)))
+            {
+                stageNum++;
+                monsterIndex = 0;
+
+                // 스킵
+                if (!GameManager.Instance.isVaildStage(new Tuple<int, int>(stageNum, monsterIndex)))
+                {
+                    Debug.LogWarning($"[스테이지 등록 실패] (stage: {stageNum}, index: {monsterIndex})는 유효하지 않은 몬스터 데이터입니다.");
+                    return; 
+                }
+            }
+
+            GameObject obj = monsterObjects[i];
+            monsters.Add(obj.transform);
+
+            MonsterController mc = obj.GetComponent<MonsterController>();
+            if (mc != null)
+            {
+                mc.stageNum = stageNum;
+                mc.monsterIndex = monsterIndex;
+
+                Debug.Log($"[몬스터 등록] 이름: {obj.name}, 스테이지: {stageNum}, 인덱스: {monsterIndex}, X: {obj.transform.position.x}");
+            }
+
+            monsterIndex++;
+        }
+
+        // 몬스터 사망 이벤트 구독 추가
+        MonsterController.IsMonsterDie -= OnMonsterDie;
+        MonsterController.IsMonsterDie += OnMonsterDie;
+    }
+
 }
