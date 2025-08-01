9d2bca4d48841122ed9d5790e37d8ea7a04e1b90
1mxn107 <uu254664@gmail.com>
2025-08-01 20:29:59 +0900

    .

--- My project/Assets/Jimin/Script/Dialogue.cs
+++ My project/Assets/Jimin/Script/Dialogue.cs
@@ -1,21 +1,25 @@
-using UnityEngine;
+using System;
 using System.Collections;
 using TMPro;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
     public TMP_Text targetText;
     public TMP_Text targetName;
 
+    public Tuple<int, int> stage;
+
     private float delay = 0.075f;
     private int index = 0;
     private bool isTyping = false;
 
-    IEnumerator DialoguePrint(int index)
-    {
+    public IEnumerator DialoguePrint(int index, int m)
+    { 
         isTyping = true;
 
-        var script = GameManager.Instance.getScript(1, true, index);
+        var script = GameManager.Instance.getScript(m, true, index);
         string speaker = script.Item1;
         string dialogue = script.Item2;
 
@@ -31,9 +35,17 @@ public class Dialogue : MonoBehaviour
         isTyping = false;
     }
 
+    /* IEnumerator TutorialDialogue(int i)
+    {
+        isTyping = true;
+    } */
+
 
     void ClickSkip()
     {
+        stage = GameManager.Instance.getStage();
+        int MainStage = stage.Item1;
+
         if (Input.touchCount > 0)
         {
             Touch touchFirst = Input.GetTouch(0);
@@ -46,7 +58,7 @@ public class Dialogue : MonoBehaviour
             {
                 if (isTyping)
                 {
-                    StopCoroutine(DialoguePrint(index));
+                    StopCoroutine(DialoguePrint(index, MainStage));
                     targetText.text = dialogue;
                     isTyping = false;
                 }
@@ -54,7 +66,7 @@ public class Dialogue : MonoBehaviour
                 if (!isTyping)
                 {
                     index++;
-                    StartCoroutine(DialoguePrint(index));
+                    StartCoroutine(DialoguePrint(index, MainStage));
                 }
             }
         }
@@ -62,10 +74,6 @@ public class Dialogue : MonoBehaviour
 
     void Start()
     {
-        if (!isTyping)
-        {
-            StartCoroutine(DialoguePrint(index));
-        }
 
     }
 
