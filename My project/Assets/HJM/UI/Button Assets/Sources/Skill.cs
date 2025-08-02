10d930ec66cbc130c36a25c17cb4a74c755f5feb
heohuhu <nicejunmin@daum.net>
2025-08-03 00:27:50 +0900

    묘기 UI 추가

---
+++ My project/Assets/HJM/UI/Button Assets/Sources/Skill.cs
@@ -0,0 +1,66 @@
+using UnityEditor.Playables;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Skill : MonoBehaviour
+{
+    public Transform parent;
+    public List<Text> Sub = new List<Text>();
+    public List<Text> ButtonText = new List<Text>();
+    void Start()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            Text buttonText = parent.GetChild(i).GetComponentInChildren<Button>().GetComponent<Text>();
+            buttonText.text = getButtonText(i);
+            ButtonText.Add(buttonText);
+            Text Sub = parent.GetChild(i).GetComponentInChildren<Text>().GetComponent<Text>();
+            Sub.text = getSubText(i);
+            this.Sub.Add(Sub);
+        }
+        
+    }
+    private string getButtonText(int index)
+    {
+        string result = "";
+        if (GameManager.Instance.Load_Skill_Cost(index, GameManager.Instance.get_Skill_level(index)) == -1)
+            result = "최대 레벨입니다.";
+        else
+            result = $"다이아몬드 {GameManager.Instance.Load_Skill_Cost(index, GameManager.Instance.get_Skill_level(index))}개";
+        return result;
+    }
+    private string getSubText(int index)
+    {
+        string result = "";
+        if (index == 7) //신이된다ㅏㅏㅏㅏ
+            result = $"8초간 데미지 누적 후 {GameManager.Instance.Load_Skill_Damage(index, GameManager.Instance.get_Skill_level(index))}% 공격\n쿨타임 {GameManager.Instance.get_Skill_Cool(index)}초";
+        else
+            result = $"{GameManager.Instance.Load_Skill_Damage(index, GameManager.Instance.get_Skill_level(index))}% 데미지 공격\n쿨타임 {GameManager.Instance.get_Skill_Cool(index)}초";
+
+        return result;
+    }
+
+    public void SkillUpgrade(int index)
+    {
+        int money = GameManager.Instance.getDiamond();
+        int cost = GameManager.Instance.Load_Skill_Cost(index, GameManager.Instance.get_Skill_level(index));
+
+        if (cost == -1) //최대레벨
+            return;
+
+        if (money < cost) //돈 부족
+            return;
+
+        money -= cost;
+        GameManager.Instance.setDiamond(money);
+        GameManager.Instance.UserData.skill_level[index]++;
+
+        Sub[index].text = getSubText(index);
+        ButtonText[index].text = getButtonText(index);
+        Debug.Log($"스킬 레벨 업그레이드 :: {GameManager.Instance.get_Skill_level(index)}레벨");
+
+    }
+}
