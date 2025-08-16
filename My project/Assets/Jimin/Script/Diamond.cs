34ff94d9651e5ee159d04fcbcbcd61a34c5b9377
1mxn107 <uu254664@gmail.com>
2025-08-17 01:58:11 +0900

    배경 크기 수정

--- My project/Assets/Jimin/Script/Diamond.cs
+++ My project/Assets/Jimin/Script/Diamond.cs
@@ -4,7 +4,7 @@ public class Diamond : MonoBehaviour
 {
     #region private field
 
-    [SerializeField] private float measurementDuration = 5f; // 측정 구간(초)
+    [SerializeField] private float measurementDuration = 30f; // 측정 구간(초)
     [SerializeField] private int baseDamage = 100;           // 100 데미지를 기준으로
     [SerializeField] private float baseTime = 1f;            // 1초 기준
     [SerializeField] private float diamondPercentage = 10f;  // 다이아 퍼센트
@@ -13,30 +13,18 @@ public class Diamond : MonoBehaviour
     private float _totalDamage;
     private bool _isMeasuring;
     private int diamond;
+    private int PuppetMaxHP;
+    private int PuppetcurrentHP;
 
     #endregion
     private void Start()
     {
-        diamond = GameManager.Instance.getDiamond();
-        Debug.Log($"[Diamond] = {diamond}");
+
     }
     private void Update()
     {
-        if (Time.time - _startTime >= measurementDuration && _isMeasuring)
-        {
-            _isMeasuring = false;
-            // _totalDamage = GameManager.Instance.
-            float elapsed = Time.time - _startTime;
-            float dps = _totalDamage / elapsed;
-            float baseDps = baseDamage / baseTime;
-            float performancePercent = (dps / baseDps) * 100f;
-            diamond = (int)(_totalDamage / diamondPercentage);
-        }
-        else if (Time.time - _startTime >= measurementDuration && !_isMeasuring)
-        {
-            _isMeasuring = true;
-            AddDiamond();
-        }
+        _totalDamage = PuppetMaxHP - PuppetcurrentHP;
+        diamond = (int)(_totalDamage / 10.0);
     }
 
     private void AddDiamond()
